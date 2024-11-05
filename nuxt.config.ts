39a4c4519b9bd6f41ec02b1d29679f768a40b4ae
Markeris <m.gzegozevskis@eif.viko.lt>
2024-11-05 16:36:56 +0200

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,7 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+
+  modules: ['@element-plus/nuxt'],
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true }
+})
